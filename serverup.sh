36b815d50d4a566c1855faaadf4d34eb7c4422ec
korchmar <korchmar@gmail.com>
2014-07-01 12:05:32 +0300

    First version
    serverup.sh - scripts for setting up new server

---
+++ serverup.sh
@@ -0,0 +1,23 @@
+echo "starting server setup"
+#apt-get update
+#apt-get install bzip2
+#apt-get install htop
+#apt-get install mc
+# ------------------ INSTALL APACHE ---------------------------
+#apt-get install apache2
+#apt-get install libapache2-mod-rpaf
+#a2enmod rewrite
+# ------------------  INSTALL PHP --------------------------------
+#apt-get install php5-common libapache2-mod-php5 php5-cli
+#apt-get install php5-mysql php5-curl
+# -------------------- INSTALL MYSQL -------------------------------
+#apt-get install mysql-server
+# ---------------------- INSTALL NGINX -----------------------------
+#apt-get install nginx
+#service apache2 restart
+#apt-get install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
+#apt-get install git
+#apt-get install emacs
+#git config --global core.editor emacs
+git config --global merge.tool vimdiff
+exit 0
